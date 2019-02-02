2b91ef3432d022a9c37b867edcfbcf917779235a
Radim.Langer <radimlanger@kiwi.com>
2019-02-02 18:52:14 +0100

    deleted main app storyboard, prepared watch extension and complication

---
+++ WatchDepartures/WatchDepartures/MainViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  WatchDepartures
+//
+//  Created by Radim Langer on 02/02/2019.
+//  Copyright © 2019 Radim Langer. All rights reserved.
+//
+
+import UIKit
+
+final class MainViewController: UIViewController {
+
+    
+}
+
